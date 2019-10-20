655670b0fa0b39c7f1d2a47481f36e4726e0e88d
Velythyl <charlie.gauthier@umontreal.ca>
2019-10-20 16:49:59 -0400

    wtf is this

--- dev2/solution.py
+++ dev2/solution.py
@@ -19,7 +19,18 @@ class SVM:
         feature_weights = features.dot(w_v)
         label_tested = coerce(feature_weights * labels)   # why le reshape necessaire?
 
-        return coerce(np.max(1 - label_tested, axis=1, initial=0))
+        maxed = np.max(1 - label_tested, axis=1, initial=0)
+
+        return coerce(maxed) # TODO pk le /10 ??????
+
+    def group_loss(self, x, y, pwr=False):
+        sums = np.zeros(self.w.shape[1])
+        for i, wi in self.for_wv_in_w():
+            loss = self.loss(wi, x, y[:, i])
+            if pwr:
+                loss = np.power(loss, 2)
+            sums[i] = np.sum(loss)
+        return sums * self.C / self.batch_size
 
     def for_wv_in_w(self):
         for i, wi in enumerate(self.w.T):
@@ -31,14 +42,18 @@ class SVM:
         y : numpy array of shape (minibatch size, 10) LABELS in -1,1
         returns : float
         """
-        sum = 0
+        sums = 0#[]
         for i, wi in self.for_wv_in_w():
-            sum += np.sum(np.power(self.loss(wi, x, y[:, i]), 2))
-        sum *= self.C / self.batch_size
+            loss = self.loss(wi, x, y[:, i])
+            pwr = np.power(loss, 2)
+            sums += np.sum(pwr) #.append(np.sum(pwr))
+        sums = self.C / self.batch_size * sums# np.array(sums)
 
-        weight_stuff = np.sum(np.power(np.linalg.norm(self.w, axis=1), 2)) / 2
+        w_norm = np.linalg.norm(self.w, axis=0)
+        w_pwr = np.power(w_norm, 2)
+        weight_stuff = np.sum(w_pwr) / 2
 
-        return weight_stuff + sum
+        return weight_stuff + np.mean(sums)
 
     def compute_gradient(self, x, y):
         """
@@ -53,10 +68,13 @@ class SVM:
 
             loss = self.loss(wi, x, labels)
 
+            temp = np.sum(loss) * self.C / self.batch_size
+
             per_feat = loss * x
             tested = coerce(labels) * per_feat
             pre_gradient = np.sum(tested, axis=0)
             gradient[i] = ((-2*self.C)/self.batch_size) * pre_gradient
+            gradient[i] += wi   #
 
         return gradient.T
 
@@ -73,7 +91,15 @@ class SVM:
         x : numpy array of shape (number of examples to infer, 401)
         returns : numpy array of shape (number of examples to infer, 10)
         """
-        pass
+        num_classes = self.w.shape[1]
+        num_data = len(x)
+
+        scores = np.zeros((num_classes, num_data))
+        for i, wi in self.for_wv_in_w():
+            scores[i] = x.dot(wi)
+
+        preds = np.argmax(scores, axis=0)
+        return self.make_one_versus_all_labels(preds, num_classes)
 
     def compute_accuracy(self, y_inferred, y):
         """
@@ -81,7 +107,9 @@ class SVM:
         y : numpy array of shape (number of examples, 10)
         returns : float
         """
-        pass
+        equals = (y_inferred == y)
+        all = np.all(equals, axis=1)
+        return np.mean(all)
 
     def fit(self, x_train, y_train, x_test, y_test):
         """
@@ -126,10 +154,8 @@ class SVM:
 
         return train_loss, train_accuracy, test_loss, test_accuracy
 
-temp = SVM(2,3,3,3,True)
-print(temp.make_one_versus_all_labels( [1, 0, 2] , 4))
-
 if __name__ == "__main__":
+
     # Load the data files
     print("Loading data...")
     x_train = np.load("train_features.npy")
@@ -137,16 +163,18 @@ if __name__ == "__main__":
     y_train = np.load("train_labels.npy")
     y_test = np.load("test_labels.npy")
 
+    svm = SVM(eta=0.001, C=30, niter=200, batch_size=5000, verbose=True)
+
     print("Fitting the model...")
-    svm = SVM(eta=0.001, C=30, niter=200, batch_size=5000, verbose=False)
-    train_loss, train_accuracy, test_loss, test_accuracy = svm.fit(x_train, y_train, x_test, y_test)
+    #train_loss, train_accuracy, test_loss, test_accuracy = svm.fit(x_train, y_train, x_test, y_test)
 
     # # to infer after training, do the following:
-    # y_inferred = svm.infer(x_test)
+    #y_inferred = svm.infer(x_test)
 
     ## to compute the gradient or loss before training, do the following:
-    # y_train_ova = svm.make_one_versus_all_labels(y_train, 10) # one-versus-all labels
-    # svm.w = np.zeros([401, 10])
-    # grad = svm.compute_gradient(x_train, y_train_ova)
-    # loss = svm.compute_loss(x_train, y_train_ova)
-
+    y_train_ova = svm.make_one_versus_all_labels(y_train, 10) # one-versus-all labels
+    svm.w = np.zeros([401, 10])
+    grad = svm.compute_gradient(x_train, y_train_ova)
+    loss = svm.compute_loss(x_train, y_train_ova)
+    print(loss)
+    print(grad)
